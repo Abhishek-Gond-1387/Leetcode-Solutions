ae87e4dc76421889d4095a08d66890d960a190f4
ABHISHEK GOND <raymiller1308@gmail.com>
2026-09-10 23:38:41 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.8%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
@@ -1,10 +1,17 @@
 class Solution(object):
     def isSameAfterReversals(self, num):
-        rev1 = list(str(num))
-        rev1.reverse()
-        rev1 = int(''.join(rev1))
+        #rev1 = list(str(num))
+        #rev1.reverse()
+        #rev1 = int(''.join(rev1))
 
-        rev2 = list(str(rev1))
-        rev2.reverse()
-        rev2 = int(''.join(rev2))
-        return rev2==num
+        #rev2 = list(str(rev1))
+        #rev2.reverse()
+        #rev2 = int(''.join(rev2))
+        #return rev2==num
+
+        #2nd var
+
+        return num == int(str(int(str(num)[::-1]))[::-1])
+
+
+
