632e9a5aad2efeab48fcd6e9cc8927a566d620dd
ABHISHEK GOND <raymiller1308@gmail.com>
2026-09-17 23:05:58 +0530

    Time: 1 ms (99.84%), Space: 46.8 MB (45.7%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,20 +1,37 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<Integer> nums = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            nums.add(i);
-        }
+        // List<Integer> nums = new ArrayList<>();
+        // for (int i = 1; i <= n; i++) {
+        //     nums.add(i);
+        // }
+
+        // List<String> li = new ArrayList<>();
+        // for (int num : nums) {
+        //     if (num % 3 == 0 && num % 5 == 0) {
+        //         li.add("FizzBuzz");
+        //     } else if (num % 3 == 0) {
+        //         li.add("Fizz");
+        //     } else if (num % 5 == 0) {
+        //         li.add("Buzz");
+        //     } else {
+        //         li.add(String.valueOf(num));
+        //     }
+        // }
+        // return li;
 
         List<String> li = new ArrayList<>();
-        for (int num : nums) {
-            if (num % 3 == 0 && num % 5 == 0) {
+        for(int i = 1; i<=n; i++){
+            if(i%15==0){
                 li.add("FizzBuzz");
-            } else if (num % 3 == 0) {
+            }
+            else if(i%3==0){
                 li.add("Fizz");
-            } else if (num % 5 == 0) {
+            }
+            else if(i%5==0){
                 li.add("Buzz");
-            } else {
-                li.add(String.valueOf(num));
+            }
+            else {
+                li.add(String.valueOf(i));
             }
         }
         return li;
